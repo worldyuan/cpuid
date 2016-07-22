97a000a2b260b30ef0099b8b7d9015210f560ede
jeek <jeekhell@gmail.com>
2016-07-22 16:30:18 +0800

    modifiy [4]uint32 to *[4]uint32

--- cpuid.go
+++ cpuid.go
@@ -1,8 +1,8 @@
 package cpuid
 
-func cpuid(info [4]uint32, ax uint32)
+func cpuid(info *[4]uint32, ax uint32)
 
-func Cpuid(info [4]uint32, ax uint32) {
+func Cpuid(info *[4]uint32, ax uint32) {
 	cpuid(info, ax)
 	return
 }
